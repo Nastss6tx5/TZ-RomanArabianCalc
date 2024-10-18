33f9740e646c53f0e8467065054a3e882516b679
Anastasiia <111811642+Nastss6tx5@users.noreply.github.com>
2024-10-18 20:54:48 +0300

    Update NumberParses.java

--- src/NumberParses.java
+++ src/NumberParses.java
@@ -1,11 +1,11 @@
-public class NumberParses {
+public class NumberRangeValidator {
     public static int parseNumber(String number) throws Exception {
         int arabicNumber = Roman.romanNum(number);
         if (arabicNumber == 0) {
             arabicNumber = Integer.parseInt(number);
         }
         if (arabicNumber < 0 || arabicNumber > 10) {
-            throw new Exception("Калькулятор работает только с числами ");
+            throw new Exception("Калькулятор работает только с числами от 1 до 10.");
         }
         return arabicNumber;
     }
